c7a94aa23201c32b5813bc29b7868ebbb906c551
mesutkoc <mestkoc34@hotmail.com>
2023-03-07 21:34:25 +0300

    connect reducer to component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { increment } from './Redux/filmSlice';
+import { fetchFilms, increment } from './Redux/filmSlice';
 
 import './App.css';
 
 function App() {
   const dispatch = useDispatch();
-  const data = useSelector((state) => state.films.value);
+  const data = useSelector((state) => state.films);
+  
+  useEffect(() => {
+    dispatch(fetchFilms())
+  }, []);
 
   return (
     <div className="App">
-      {data}
       <button
         onClick={() => dispatch(increment())}
       >+</button>
--- src/Redux/filmSlice.js
+++ src/Redux/filmSlice.js
@@ -1,8 +1,13 @@
-import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const fetchFilms = createAsyncThunk('films/fetchFilms', () => {
+  return axios.get('http://www.omdbapi.com/?apikey=7bf50757&s=thor').then((response) => response.data)
+})
 
 const initialState = {
-  value: 0,
-  status: 'idle',
+  loading: false,
+  films: {}
 };
 
 export const filmSlice = createSlice({
@@ -13,11 +18,18 @@ export const filmSlice = createSlice({
       state.value += 1;
     },
   },
-
+  extraReducers: (builder) => {
+    builder.addCase(fetchFilms.pending, (state) => {
+      state.loading = true;
+    })
+    builder.addCase(fetchFilms.fulfilled, (state, action) => {
+      state.loading = false;
+      state.films = action.payload;
+      state.error = '';
+    })
+  }
 });
 
 export const { increment } = filmSlice.actions;
-
-
 
 export default filmSlice.reducer;
