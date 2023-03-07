6c8efb374a9fd872fda5d4696609b1d1a6baade4
mesutkoc <mestkoc34@hotmail.com>
2023-03-07 22:02:05 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { useEffect } from 'react';
-
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchFilms, increment } from './Redux/filmSlice';
+import { fetchFilms } from './Redux/filmSlice';
 
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.films);
-  
+
   useEffect(() => {
     dispatch(fetchFilms())
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="App">
-      <button
-        onClick={() => dispatch(increment())}
-      >+</button>
+      <Header></Header>
+      
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./header.scss";
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="title">FILM ANALYTICS
+
+            </div>
+        </header>
+    )
+}
+
+export default Header;
